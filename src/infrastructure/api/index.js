e0d7dc3c4315e136c9143f85b67a2e2482bb0789
Felipe Ulloa <felipeulloa5a43@gmail.com>
2023-06-11 23:56:36 -0400

    feat: :sparkles: first version endpoint documentation in swagger

--- src/infrastructure/api/index.js
+++ src/infrastructure/api/index.js
@@ -3,9 +3,26 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import adminRoutes from "./routes/admin.routes.js";
 import sensorRoutes from "./routes/sensor.routes.js";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API de ejemplo",
+      version: "1.0.0",
+      description: "Una API creada usando Node.js, Express y sqlite",
+    },
+  },
+  apis: ["src/infrastructure/api/routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(bodyParser.json());
 app.use(cors());
